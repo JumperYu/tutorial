8203b1be86a67b758693ff614fb484c223eaa55c
xiangmin.zeng <xiangmin.zeng@vistel.cn>
2019-07-05 18:30:09 +0800

    [ADD] init

--- zipkin-demo/zipkin-web/src/main/java/com/bradypod/ZipkinApplication.java
+++ zipkin-demo/zipkin-web/src/main/java/com/bradypod/ZipkinApplication.java
@@ -2,13 +2,11 @@ package com.bradypod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin2.server.internal.EnableZipkinServer;
 
 /**
  * @author xiangmin.zeng@vistel.cn
  * @version 1.0    2019/7/4
  */
-@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinApplication {
     public static void main(String[] args) {
